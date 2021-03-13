22f4c99da16c63c7e4e6bd9782f15a5cc3533b14
Andy Hablewitz <mail@andyhablewitz.com>
2021-03-13 16:41:36 -0700

    wip - detangling promises lol

--- src/Slider.js
+++ src/Slider.js
@@ -13,20 +13,41 @@ SwiperCore.use([Navigation, Pagination, Virtual]);
 class Slider extends Component{
     constructor(){
         super();
-        this.state = { topStories: [], storyData: []}
+        this.state = { topStories: [], storyData: [], currentOffset: 10, storiesToLoad: 10}
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+
+        const topStories = await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`);
+        console.log("TOP STORIES");
+        console.log(await topStories.json());
+
         fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)
             .then(res => res.json())
-            .then(json => this.setState({ topStories: json }));
+            .then(json => {
+                this.setState({ topStories: json });
+                var currentItems = this.state.topStories.slice(this.state.currentOffset - this.state.storiesToLoad, this.state.currentOffset);
+                var itemsArray = [];
+                currentItems.forEach(x => {
+                    fetch(`https://hacker-news.firebaseio.com/v0/item/${x}.json`)
+                        .then(res => res.json())
+                        .then(json => {
+                            itemsArray.push(json);
+                    });
+                });
+
+                console.log(itemsArray);
+                this.setState({ storyData: itemsArray });
+
+            });
     }
 
     render() {
-
-        if( this.state.topStories.length > 0){
-            const list = this.state.topStories.map(story => <SwiperSlide key={story} id={story}><div className="hacker-card">{story}</div></SwiperSlide> );
-
+        console.log('render!');
+        if( this.state.storyData.length > 0){
+            const list = this.state.storyData.map(story => <SwiperSlide key={story} id={story.id}><div className="hacker-card">{story.title}</div></SwiperSlide> );
+            console.log('logging list');
+            console.log(list);
             return (
                 <div className='sliderContainer'>
                     
