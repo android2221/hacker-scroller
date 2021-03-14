a4582b40edd69f59f57075936d36f5a912ce34c1
Andy Hablewitz <mail@andyhablewitz.com>
2021-03-13 21:18:32 -0700

    loading animation

--- src/Slider.js
+++ src/Slider.js
@@ -2,6 +2,7 @@
 import SwiperCore, { Navigation, Pagination, Virtual } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import React, { Component } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 // Import Swiper styles
 import 'swiper/swiper.scss';
@@ -13,21 +14,31 @@ SwiperCore.use([Navigation, Pagination, Virtual]);
 class Slider extends Component{
     constructor(){
         super();
-        this.state = { topStories: [], storyData: [], currentOffset: 10, storiesToLoad: 10}
+        this.state = { topStories: [], 
+                       storyData: [], 
+                       currentStoriesSlice: [],
+                       currentOffset: 10, 
+                       storiesToLoad: 10,
+                       loading: true,
+                    }
     }
 
     async componentDidMount() {
 
         const topStoriesResult = await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`);
         const topStoriesJson = await topStoriesResult.json();
+        const currentStoriesSlice = topStoriesJson.slice(0, this.state.currentOffset);
 
-        var storyDataArray = await Promise.all(topStoriesJson.map(async (x) => {
+        var storyDataArray = await Promise.all(currentStoriesSlice.map(async (x) => {
             var result = await fetch(`https://hacker-news.firebaseio.com/v0/item/${x}.json`);
             return result.json();
         }));
 
-        this.setState({topStories: topStoriesJson, storyData: storyDataArray});
-        
+        this.setState({topStories: topStoriesJson, 
+                       storyData: storyDataArray, 
+                       currentStoriesSlice: currentStoriesSlice,
+                       loading: false
+                      });
     }
 
     render() {
@@ -36,8 +47,6 @@ class Slider extends Component{
             const list = this.state.storyData.map(story => {
                 return <SwiperSlide key={story.id} id={story.id}><div className="hacker-card">{story.title}</div></SwiperSlide>
             });
-            console.log('logging list');
-            console.log(list);
             return (
                 <div className='slider-container'>
                     <Swiper
@@ -49,11 +58,17 @@ class Slider extends Component{
                         onSwiper={(swiper) => console.log(swiper)}>
                         {list}
                     </Swiper>
+                    <div id='loading-icon' className='loading'>
+                        <ClipLoader size={35} color='black' loading={this.state.loading} />
+                    </div>
                 </div>
-
             );
         }
-        return("Nothing here")
+            return(
+                <div id='loading-icon' className='loading'>
+                    <ClipLoader size={35} color='black' loading={this.state.loading} />
+                </div>
+            );
         }
 }
 
