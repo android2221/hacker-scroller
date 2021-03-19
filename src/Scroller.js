6ad5ba0dba60ab262860fd04aa16bcfec2dbfd84
Andy Hablewitz <mail@andyhablewitz.com>
2021-03-19 16:37:57 -0600

    styling improvements to prevent hiding last comment and weird 100vh issues

--- src/Scroller.js
+++ src/Scroller.js
@@ -106,7 +106,7 @@ class Scroller extends Component {
                             {story.score} points by {story.by} | {story.descendants ? story.descendants : 0} comments | <Moment unix fromNow>{story.time}</Moment>
                         </div>
                         <div className='top-comments'>
-                            <h3>Top Comments</h3>
+                            {topComments !== undefined ? <h3>Top Comments</h3> : ''}
                             {topComments !== undefined ? topComments.map((comment, index) =>
                                 <div className="comment" key={index}>
                                     <div className="comment-by">{comment.by} said:</div>
