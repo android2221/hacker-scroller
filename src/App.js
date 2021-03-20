ad2cf6e07130a6ec7daa5f426f78df7cae82688d
Andy Hablewitz <mail@andyhablewitz.com>
2021-03-19 20:21:43 -0600

    added header bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Scroller from './Scroller';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <div id="header">
+        <div className="site-name">
+          Hacker Scroller
+        </div>
+        <div className="about">
+          <a href="https://github.com/android2221/hacker-scroller">GitHub Project</a>
+        </div>
+      </div>
       <Scroller></Scroller>
     </div>
   );
